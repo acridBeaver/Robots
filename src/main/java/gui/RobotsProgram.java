0c6fba19ed0d07f3fff4c80b8dadba37571e44ef
Pavel Slabikov <spv@renue.ru>
2021-04-20 21:41:07 +0500

    Improved serialization feature

--- src/main/java/gui/RobotsProgram.java
+++ src/main/java/gui/RobotsProgram.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.Frame;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
--- src/main/java/serializer/WindowPresetToDatConverter.java
+++ src/main/java/serializer/WindowPresetToDatConverter.java
@@ -5,11 +5,12 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Optional;
 
 public class WindowPresetToDatConverter {
-    private static final String DEFAULT_PRESET_FOLDER = "presets/";
+    private static final Path DEFAULT_DIR = Path.of("presets");
 
     private final Serializer<WindowPreset> presetSerializer;
     private final Deserializer<WindowPreset> presetDeserializer;
@@ -21,21 +22,32 @@ public class WindowPresetToDatConverter {
     }
 
     public void convertToFile(String filename, WindowPreset preset) {
-        File file = new File(DEFAULT_PRESET_FOLDER + filename);
         try {
-            file.createNewFile();
-            presetSerializer.serialize(preset, new FileOutputStream(file));
+            if (!Files.exists(DEFAULT_DIR)) {
+                Files.createDirectory(DEFAULT_DIR);
+            }
+
+            Path pathToPreset = DEFAULT_DIR.resolve(filename);
+            File presetFile = pathToPreset.toFile();
+            presetFile.createNewFile();
+            presetSerializer.serialize(preset, new FileOutputStream(presetFile));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public Optional<WindowPreset> getFromFile(String filename) {
-        File file = new File(DEFAULT_PRESET_FOLDER + filename);
+        File file = DEFAULT_DIR.resolve(filename).toFile();
         try {
             return Optional.of(presetDeserializer.deserialize(new FileInputStream(file)));
         } catch (IOException | ClassNotFoundException e) {
             return Optional.empty();
         }
+    }
+
+    public boolean hasPresets() {
+        File presetFolder = DEFAULT_DIR.toFile();
+        return presetFolder.isDirectory()
+                && Objects.requireNonNullElse(presetFolder.list(), new String[0]).length > 0;
     }
 }
