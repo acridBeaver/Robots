92bbb595a19a1dbc2394d9c828d161bc0921f988
Pavel Slabikov <spv@renue.ru>
2021-05-19 13:12:45 +0500

    Changed import

--- src/main/java/serializer/MainFrameSerializer.java
+++ src/main/java/serializer/MainFrameSerializer.java
@@ -1,13 +1,13 @@
 package serializer;
 
-import gui.MainApplicationFrame;
+import gui.windows.MainWindow;
 import gui.model.WindowPreset;
 import java.util.Optional;
 
 public class MainFrameSerializer {
   private static final WindowPresetConverter converter = new WindowPresetConverter();
 
-  public static void serialize(MainApplicationFrame frame) {
+  public static void serialize(MainWindow frame) {
     WindowPreset mainPreset = new WindowPreset();
     mainPreset.setLocation(frame.getLocation());
     mainPreset.setMinimized(false);
@@ -15,7 +15,7 @@ public class MainFrameSerializer {
     converter.convertToFile("Main Window", mainPreset);
   }
 
-  public static void deserialize(MainApplicationFrame frame) {
+  public static void deserialize(MainWindow frame) {
     Optional<WindowPreset> presetWrapper = converter.getFromFile("Main Window");
     if (presetWrapper.isEmpty()) {
       return;
